7044caf483f75c08e0308431f8f17f6695e24490
Jack Lynch <jacklynch@wharnal.com>
2014-02-12 21:50:08 -0500

    test commit for new branch

---
+++ Hans_Mobile/src/com/hans/runner/Hans.java
@@ -0,0 +1,17 @@
+package com.hans.runner;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Hans {
+	
+	Hans(World world, int x, int y) {
+		
+	}
+
+}
--- Hans_Mobile/src/com/hans/runner/HansMobile.java
+++ Hans_Mobile/src/com/hans/runner/HansMobile.java
@@ -2,26 +2,35 @@ package com.hans.runner;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class HansMobile implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+	private World world;
+	private Box2DDebugRenderer debugRenderer;
 	
 	@Override
-	public void create() {		
+	public void create() {
+		
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
+		camera = new OrthographicCamera(30.0f, 20.0f);/*
 		batch = new SpriteBatch();
 		
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
@@ -33,6 +42,41 @@ public class HansMobile implements ApplicationListener {
 		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
 		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
 		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		*/
+		
+		world = new World(new Vector2(0, -10), true); 
+		debugRenderer = new Box2DDebugRenderer();
+
+
+		// First we create a body definition
+		BodyDef bodyDef = new BodyDef();
+		// We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+		bodyDef.type = BodyType.DynamicBody;
+		// Set our body's starting position in the world
+		bodyDef.position.set(100, 300);
+
+		// Create our body in the world using our body definition
+		Body body = world.createBody(bodyDef);
+
+		// Create a circle shape and set its radius to 6
+		CircleShape circle = new CircleShape();
+		circle.setRadius(6f);
+
+		// Create a fixture definition to apply our shape to
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 0.5f; 
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+
+		// Create our fixture and attach it to the body
+		Fixture fixture = body.createFixture(fixtureDef);
+
+		// Remember to dispose of any shapes after you're done with them!
+		// BodyDef and FixtureDef don't need disposing, but shapes do.
+		circle.dispose();
+
+
 	}
 
 	@Override
@@ -43,6 +87,7 @@ public class HansMobile implements ApplicationListener {
 
 	@Override
 	public void render() {		
+		/*
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
@@ -50,6 +95,12 @@ public class HansMobile implements ApplicationListener {
 		batch.begin();
 		sprite.draw(batch);
 		batch.end();
+		*/
+		
+		debugRenderer.render(world, camera.combined);
+
+		world.step(1/60f, 6, 2);
+
 	}
 
 	@Override
