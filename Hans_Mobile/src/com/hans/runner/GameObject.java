1bf9255438f67267cc3c327c892b30f60d438fab
Jack Lynch <jacklynch@wharnal.com>
2014-02-15 12:56:42 -0500

    added getter/setters to GameObject, started update function

--- Hans_Mobile/src/com/hans/runner/GameObject.java
+++ Hans_Mobile/src/com/hans/runner/GameObject.java
@@ -4,10 +4,46 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.Body;
 
 public class GameObject {
-	int x;
-	int y;
-	Texture sprite;
-	Body physicsObject;
+	private int x;
+	private int y;
+	private Texture sprite;
+	private Body physicsObject;
+	
+	GameObject(int x, int y, Texture sprite, Body physicsObject) {
+		this.x = x;
+		this.y = y;
+		this.sprite = sprite;
+		this.physicsObject = physicsObject;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public Texture getSprite() {
+		return sprite;
+	}
+	public void setSprite(Texture sprite) {
+		this.sprite = sprite;
+	}
+	public Body getPhysicsObject() {
+		return physicsObject;
+	}
+	public void setPhysicsObject(Body physicsObject) {
+		this.physicsObject = physicsObject;
+	}
+	
+	public void update() {
+		x = physicsObject.getPosition() //TODO: convert vector to x/y coordinates
+	}
 	
 
 }
